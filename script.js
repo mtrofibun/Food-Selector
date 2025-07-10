136a2bed969720b936c2b6e46a132c2dd26d9fd2
NinaTrofimov <ntrofimov1@my.waketech.edu>
2025-07-10 11:14:08 -0400

    Start

---
+++ script.js
@@ -0,0 +1,82 @@
+foods = ['Chick-Fil-a','McDonalds','Chex Wing and Grill','Sabor Latin Grill','Papa Johns','Cook-Out','Kaizoku','WingStop']
+
+
+
+food [
+    {
+    chain : 'Papa Johns',
+    type : 'Pizza',
+    minPrice : 10.00,
+    maxPrice : 15.00
+},
+{
+    chain : 'Chick-Fil-A',
+    type : 'Sandwich/Sub',
+    minPrice : 10.00,
+    maxPrice : 11.00
+},
+{
+    chain : 'McDonalds',
+    type : 'Snack',
+    minPrice : 7.00,
+    maxPrice : 11.00
+},
+{
+    chain : 'Hungry Howies',
+    type : 'Pizza',
+    minPrice : 10.00,
+    maxPrice : 13.00
+},
+{
+    chain : `Jet's Pizza`,
+    type : 'Pizza',
+    minPrice : 12.00,
+    maxPrice : 15.00
+},
+{
+    chain : 'New York Pizza and Pasta',
+    type : 'Pizza',
+    minPrice : 10.00,
+    maxPrice : 20.00
+},
+{
+    chain : 'Chex Wing and Grill',
+    type : 'Sandwich/Sub',
+    minPrice : 10.00,
+    maxPrice : 14.00
+},
+{
+    chain : 'Sabor',
+    type : 'Burgers/Beef',
+    minPrice : 8.00,
+    maxPrice : 11.00
+},
+{
+    chain : 'CookOut',
+    type : 'Burgers/Beef',
+    minPrice : 6.00,
+    maxPrice : 10.00
+},
+{
+    chain : 'Kaizoku',
+    type : 'Non-American Cusine',
+    minPrice : 12.00,
+    maxPrice : 17.00
+},
+{
+    chain : 'ShakeShack',
+    type : 'Burgers/Beef',
+    minPrice : 7.00,
+    maxPrice : 14.00
+}
+
+
+
+
+
+
+]
+
+document.addEventListener('click', function(){
+    
+})
